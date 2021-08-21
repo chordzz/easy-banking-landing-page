36bd70ddc76073acb2229b6d9ddc1789b224dbc6
Olaife Olawore <olaifeolawore@gmail.com>
2021-08-21 07:44:00 +0100

    site revamp mobile and desktop

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,9 +1,9 @@
 function toggleMenu() {
-  document.getElementById("navList").classList.toggle("open");
-  document.querySelector(".intro-container").classList.toggle("background");
+  document.getElementById("nav-list").classList.toggle("open");
+  document.querySelector("#intro-section").classList.toggle("background");
   document.querySelector(".hamburger").classList.toggle("active");
   document.querySelector(".close").classList.toggle("active");
 }
 
-let x = document.getElementById("hamburgerIcon");
+let x = document.getElementById("nav-icon-div");
 x.onclick = toggleMenu;
